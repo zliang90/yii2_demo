3acae13523ef30e252939a66e5f781254f47db97
zhangliang5 <zhangliang5@xiaomi.com>
2017-06-21 17:42:51 +0800

    add Country from db

--- controllers/CountryController.php
+++ controllers/CountryController.php
@@ -29,7 +29,7 @@ class CountryController extends Controller
             ->all();
 
         return $this->render("index", [
-            'country' => $countries,
+            'countries' => $countries,
             'pagination' => $pagination,
         ]);
     }
