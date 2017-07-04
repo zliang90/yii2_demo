c6f6c96db283f7b77bf4972074f235136a70be43
zhangliang5 <zhangliang5@xiaomi.com>
2017-07-04 10:34:48 +0800

    add console Controller

--- commands/HelloController.php
+++ commands/HelloController.php
@@ -7,7 +7,9 @@
 
 namespace app\commands;
 
+use Yii;
 use yii\console\Controller;
+use yii\data\SqlDataProvider;
 
 /**
  * This command echoes the first argument that you have entered.
@@ -30,5 +32,29 @@ class HelloController extends Controller
 
     public function actionSay($message = 'hello world') {
         echo $message . "\n";
+    }
+
+    public function actionGetUsers($size = 10){
+        $count = Yii::$app->db->createCommand('SELECT COUNT(1) FROM country')->queryScalar();
+
+        $provider = new SqlDataProvider([
+            'sql' => 'SELECT * FROM country',
+            'totalCount' => $count,
+            'pagination' => [
+                'pageSize' => $size > 0 ? $size : 10,
+            ],
+            'sort' => [
+                'attributes' => [
+                    'id',
+                    'name',
+                    'email',
+                ],
+            ],
+        ]);
+
+        // returns json string of data rows
+        $users = $provider->getModels();
+
+        echo json_encode($users);
     }
 }
--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=yii2demo',
-    'dsn' => 'mysql:host=10.112.72.150;port=3306;dbname=yii2demo',
-//    'username' => 'root',
-    'username' => 'test',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=yii2demo',
+//    'dsn' => 'mysql:host=10.112.72.150;port=3306;dbname=yii2demo',
+    'username' => 'root',
+//    'username' => 'test',
     'password' => '123456',
     'charset' => 'utf8',
 ];
